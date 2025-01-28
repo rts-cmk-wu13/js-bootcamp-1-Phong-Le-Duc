8e8b78d4d6b09812574b1490a4e1d251c3bb92e3
Phong <phongleduc@gmail.com>
2025-01-28 09:39:29 +0100

    array blockQuotes added

---
+++ script.js
@@ -0,0 +1,4 @@
+let blockQuotes = ["quote1", "quotes2", "quote3", "quotes4", "quotes5", "quote6", "quote7", "quote8", "quote9", "quote10"];
+let quotesListElm = document.querySelector(".quotes-list");
+
+console.log(quotesListElm);
